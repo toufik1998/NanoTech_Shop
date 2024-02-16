8c8af2471b278d8d9c975bcd4a5a2c379330eee3
toufik1998 <toufikshima98@gmail.com>
2024-02-16 09:19:57 +0100

    add item count badge to the cart icon in the header section

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
+import {  Badge, Navbar, Nav, Container } from 'react-bootstrap'
 import { LinkContainer} from 'react-router-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import  logo from '../assets/logo.png' 
 
 const Header = () => {
+    const { cartItems } = useSelector((state) => state.cart)
   return (
     <header>
         <Navbar bg="dark" variant='dark' expand="md" collapseOnSelect>
@@ -21,6 +23,13 @@ const Header = () => {
                         <LinkContainer to='/cart'>
                             <Nav.Link >
                                 <FaShoppingCart/> Cart
+                                {
+                                    cartItems.length > 0 && (
+                                        <Badge pill style={{ marginLeft: '5px' }}>
+                                            { cartItems.reduce((a, c) => a + c.qty, 0) }
+                                        </Badge>
+                                    )
+                                }
                             </Nav.Link>
                         </LinkContainer>
                         <LinkContainer to='/sign in'>
