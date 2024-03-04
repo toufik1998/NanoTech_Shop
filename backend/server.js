fb8cb4d75892226e02b6972fa4cb2c617f172738
toufik1998 <toufikshima98@gmail.com>
2024-03-04 14:45:03 +0100

    Implement the logic for the login functionality with jwt and create an authentication middleware to protect routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMidleware.js';
@@ -10,6 +11,13 @@ const port = process.env.PORT || 1234;
 
 const app = express();
 connectDB(); // connect to database
+
+// Body Parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send("APIIIIIIIIIIIIIIIIIIII ")
