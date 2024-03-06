8c7db7d525411b325411ebfad8a36fc5db83e5d8
toufik1998 <toufikshima98@gmail.com>
2024-03-06 10:31:11 +0100

    initialize and work on auth slice, users api slice

---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,17 @@
+import { USERS_URl } from "../constants"; 
+import { apiSlice } from "./apiSlice"; 
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        login: builder.mutation({
+            query: (data) => ({
+                url: USERS_URl,
+                method: 'POST',
+                body: data,
+            }),
+        }),
+
+    }),
+});
+
+export const { useLoginMutation } = usersApiSlice;
