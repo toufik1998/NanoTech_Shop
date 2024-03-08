279299c9dfad356148b32d163c25455a6ea6684a
toufik1998 <toufikshima98@gmail.com>
2024-03-08 12:21:54 +0100

    Implement functionality for adding order items, retrieving my orders, and retrieving an order by its ID

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,39 @@ import Order from "../models/orderModel.js";
 // @route   POST /api/orders
 // @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
-    res.send('addOrderItems');
+    const { 
+        orderItems, 
+        shippingAddress, 
+        paymentMethod, 
+        itemsPrice, 
+        taxPrice, 
+        shippingPrice, 
+        totalPrice 
+    } = req.body;
+
+    if(orderItems && orderItems.length === 0) {
+        res.status(400);
+        throw new Error('No order items');
+    } else {
+        const order = new Order({
+            orderItems: orderItems.map((x) => ({
+                ...x,
+                product: x._id,
+                _id: undefined,
+            })), 
+            user: req.user._id,
+            shippingAddress, 
+            paymentMethod, 
+            itemsPrice, 
+            taxPrice, 
+            shippingPrice, 
+            totalPrice 
+        });
+
+        const createdOrder = await order.save();
+
+        res.status(201).json(createdOrder);
+    }
 });
 
 
@@ -13,7 +45,8 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @route   POST /api/orders/myorders
 // @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
-    res.send('get my orders');
+    const orders = await Order.find({ user: req.user._id });
+    res.status(200).json(orders);
 });
 
 
@@ -21,7 +54,14 @@ const getMyOrders = asyncHandler(async (req, res) => {
 // @route   POST /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
-    res.send('get  order by id');
+    const order = await Order.findById({ id: req.params.id }).populate('user', 'name email');
+
+    if (order) {
+        res.status(200).json(order);
+    }else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
 });
 
 
