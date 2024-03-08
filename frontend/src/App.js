19379045d7daa93cf6e6d2c7292b03aa1e37c351
toufik1998 <toufikshima98@gmail.com>
2024-03-08 11:11:32 +0100

    Create and design the payment screen, and implement the functionality to save the payment method.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,9 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 import PrivateRoute from './components/PrivateRoute';
+
 
 const App = () => {
   return (
@@ -29,6 +31,7 @@ const App = () => {
 
               <Route path="/" element={<PrivateRoute />}>
                 <Route path="/shipping" element={<ShippingScreen />} />
+                <Route path="/payment" element={<PaymentScreen />} />
               </Route>
 
 
