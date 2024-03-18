1672cc584ecb02069953597a71af15509af9b640
toufik1998 <toufikshima98@gmail.com>
2024-03-18 11:59:05 +0000

    Create the user list screen and display all user information

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,7 @@ import AdminRoute from './components/AdminRoute';
 import OrderListScreen from './screens/admin/OrderListScreen';
 import ProductListScreen from './screens/admin/ProductListScreen';
 import ProductEditScreen from './screens/admin/ProductEditScreen';
+import UserListScreen from './screens/admin/UserListScreen';
 
 
 
@@ -49,6 +50,7 @@ const App = () => {
                 <Route path="/admin/orderlist" element={<OrderListScreen />} />
                 <Route path="/admin/productlist" element={<ProductListScreen />} />
                 <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
+                <Route path="/admin/userlist" element={<UserListScreen />} />
                </Route> 
 
             </Routes>
--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -30,7 +30,17 @@ export const usersApiSlice = apiSlice.injectEndpoints({
                 body: data,
             }),
         }),
+        getUsers: builder.query({
+            query: () => ({
+                url: `${USERS_URl}`,
+                method: 'GET',
+            }),
+            providesTags: ['User'],
+            keepUnusedDataFor: 5,
+        }),
+
+
     }),
 });
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation, useProfileMutation } = usersApiSlice;
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation, useProfileMutation, useGetUsersQuery } = usersApiSlice;
