8bb06ffbaa372954a827e18028999fa9f1f5bbef
toufik1998 <toufikshima98@gmail.com>
2024-03-19 10:55:22 +0000

    add the paginate component in the admin section

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,14 +1,18 @@
 import { LinkContainer } from "react-router-bootstrap"
 import { Table, Button, Row, Col } from "react-bootstrap"
 import { FaTimes, FaEdit, FaTrash } from "react-icons/fa"
+import { useParams } from "react-router-dom"
 import {toast} from "react-toastify"
 import Message from "../../components/Message"
 import Loader from "../../components/Loader"
+import Paginate from "../../components/Paginate"
 import { useGetProductsQuery, useCreateProductMutation, useDeleteProductMutation } from "../../slices/productsApiSlice"
 
 
 const ProductListScreen = () => {
-    const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+    const { pageNumber } = useParams();
+
+    const { data, isLoading, error, refetch } = useGetProductsQuery({pageNumber});
     
     const [createProduct, {isLoading: loadingCreate }] = useCreateProductMutation();
 
@@ -70,7 +74,7 @@ const ProductListScreen = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {products.map(product => (
+                        {data.products.map(product => (
                             <tr key={product._id}>
                                 <td>{product._id}</td>
                                 <td>{product.name}</td>
@@ -91,6 +95,11 @@ const ProductListScreen = () => {
                         ))}
                     </tbody>
                 </Table>
+                <Paginate 
+                    pages={data.pages} 
+                    page={data.page} 
+                    isAdmin={true}
+                />
             </>
         )}
     </>
