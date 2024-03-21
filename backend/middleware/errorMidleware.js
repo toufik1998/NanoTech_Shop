af9d0f9e0f99e7e9ee2243db5d9802c8263904e7
toufik1998 <toufikshima98@gmail.com>
2024-03-21 09:46:24 +0000

    Implement checkObjectId functionality to validate Mongoose object IDs

--- backend/middleware/errorMidleware.js
+++ backend/middleware/errorMidleware.js
@@ -8,11 +8,7 @@ const errorHandler = (err, req, res, next) => {
     let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     let message = err.message;
 
-    // check for mongoose bad ObjectId
-    if(err.name === 'CastError' && err.kind === 'ObjectId') {
-        message = 'resource not found';
-        statusCode = 404;
-    }
+    
 
     res.status(statusCode).json({ 
         message,
